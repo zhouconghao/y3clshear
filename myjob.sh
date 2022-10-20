e5a7fc284030fb2e03c9888ddbe24860689e2420
Conghao Zhou <zchusre@gmail.com>
2022-10-19 19:20:50 -0700

    restructure folder

--- myjob.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH --constraint=haswell
-#SBATCH --nodes=3
-#SBATCH --time=5
-
-module load python
-srun -n 96 -c 2 python mympi.py
--- mympi.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-from mpi4py import MPI
-
-mpi_rank = MPI.COMM_WORLD.Get_rank()
-mpi_size = MPI.COMM_WORLD.Get_size()
-print(mpi_rank, mpi_size)
